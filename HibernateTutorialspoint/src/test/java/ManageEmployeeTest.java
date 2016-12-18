b9f2ef3585deb1968dd8ca7bca2381e69661f216
sschlorf <sschlorf@yahoo.de>
2016-12-18 10:44:04 +0100

    add test content

--- HibernateTutorialspoint/src/test/java/ManageEmployeeTest.java
+++ HibernateTutorialspoint/src/test/java/ManageEmployeeTest.java
@@ -1,31 +1,20 @@
 
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 public class ManageEmployeeTest extends TestCase {
-	/**
-	 * Create the test case
-	 *
-	 * @param testName
-	 *            name of the test case
-	 */
 	public ManageEmployeeTest(String testName) {
 		super(testName);
 	}
 
-	/**
-	 * @return the suite of tests being tested
-	 */
 	public static Test suite() {
 		return new TestSuite(ManageEmployeeTest.class);
 	}
 
-	/**
-	 * Rigourous Test :-)
-	 */
-	public void testApp() {
-		assertTrue(true);
+	public void addEmployeeTest() {
+		assertTrue("Check that an employee is added and those id is delivered", new ManageEmployee().addEmployee("TestEmployee", "TestEmployeeLongName", 10000) > 0);
 	}
 
 }
